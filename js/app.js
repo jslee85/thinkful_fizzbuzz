a732cec47823208a90b6b3880e0790750f5eb73e
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-08-10 23:25:58 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	for (var i = 1; i <= 100; i++) {
+		if ( ([i]%3) == 0 ) {
+			$('body').append('<div>Fizz</div>');
+		}
+		else if ( ([i])%5 == 0 ) {
+			$('body').append('<div>Buzz</div>');
+		}
+		else {
+			$('body').append('<div>'+[i]+'</div>');
+		}
+	}
+});
