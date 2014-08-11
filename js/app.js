334981ba47063a713e418311f69833010cd4e0a3
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-08-10 23:32:05 -0700

    Add fizz buzz

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	for (var i = 1; i <= 100; i++) {
-		if ( ([i]%3) == 0 ) {
-			$('body').append('<div>Fizz</div>');
+		if ( ([i]%15) == 0 ) {
+			$('body').append('<div>Fizz buzz</div>');
 		}
+		else if ( ([i])%3 == 0 ) {
+			$('body').append('<div>Fizz</div>');
+		}		
 		else if ( ([i])%5 == 0 ) {
 			$('body').append('<div>Buzz</div>');
 		}
