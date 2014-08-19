be7c01cf8c6b728076875d8afb2b264866239ccb
Jonathan Lee <Jonathan@macmini-wifi.omghax.biz>
2014-08-18 18:24:11 -0700

    add third equal sign

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	for (var i = 1; i <= 100; i++) {
-		if ( ([i]%15) == 0 ) {
+		if ( ([i]%15) === 0 ) {
 			$('body').append('<div>Fizz buzz</div>');
 		}
-		else if ( ([i])%3 == 0 ) {
+		else if ( ([i])%3 === 0 ) {
 			$('body').append('<div>Fizz</div>');
 		}		
-		else if ( ([i])%5 == 0 ) {
+		else if ( ([i])%5 === 0 ) {
 			$('body').append('<div>Buzz</div>');
 		}
 		else {
